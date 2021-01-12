940711bdc4b51170b406287794a60fe3e47cb20f
이석민 <seokmin@iseogmin-ui-MacBookPro.local>
2021-01-12 12:17:02 +0900

    “footer_add”

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,27 @@
-$(document).ready(function() {
-    $('#body').css('background-color', '#b7a786')
-    $('.file-upload').css('background-color', '#bea96c')
-    $('.drag-text').css('background-color', '#bbbbbb')
-    $('#close').click(function() {
-        $('#popup').hide()
-        $('#body').css('background-color', '#f0dbb0')
-        $('.file-upload').css('background-color', '#f0d689')
-        $('.drag-text').css('background-color', '#ffffff')
+$(document).ready(function () {
+    $('#close').click(function () {
+        $('#popup').hide();
+        $('#mask').hide();
+        $('#body').css('background-color', '#ffffff');
+        $('.file-upload').css('background-color', '#ffffff')
     })
 })
+
+function result_img_popup() {
+    $('#result_btn').click(function () {
+        $('#popup').css('display', 'block');
+        // $('#body').css('background-color', '#808080');
+        // $('.file-upload').css('background-color', '#808080')
+        //화면의 높이와 너비를 구한다.
+        var maskHeight = $(document).height();
+        var maskWidth = $(window).width();
+
+        //마스크의 높이와 너비를 화면 것으로 만들어 전체 화면을 채운다.
+        $('#mask').css({
+            'width': maskWidth,
+            'height': maskHeight
+        });
+        $('#mask').fadeIn();
+        $('#popup').css('opacity', '1')
+    })
+}
